047dcde58dea7758f3e49e0af3beec16997df5b0
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-04-21 15:35:15 -0400

    Added Routing

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { 
 
 module.exports = {
   mode: env,
+  output: { publicPath: '/' },
   entry: ['./src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
   module: {
@@ -75,5 +76,6 @@ module.exports = {
   ],
   devServer: {
     hot: true,
+    historyApiFallback: true,
   },
 };
