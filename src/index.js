5a4a64be91e1464304fb25280d7d143d9628b27a
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-04-21 15:56:59 -0400

    updated index.js with boolean for exact

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function Nav(props) {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/" exact="true">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/test/id1">test id1</NavLink></li>
         <li><NavLink to="/test/id2">test id2</NavLink></li>
