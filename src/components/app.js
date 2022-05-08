812d2938292e6e16bad822d693022be732431b41
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-05-08 03:19:39 -0400

    initial search banner

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Routes, NavLink, useParams,
 } from 'react-router-dom';
-import Counter from './counter';
-import Controls from './control';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import { SearchRounded } from '@mui/icons-material';
+// import Counter from './counter';
+// import Controls from './control';
 
 function About(props) {
   return <div> All there is to know about me </div>;
@@ -11,9 +14,21 @@ function About(props) {
 function Welcome(props) {
   return (
     <div>
-      <Counter />
-      <Controls />
-      <div>Welcome</div>
+      <section className="city-search-banner">
+        <form className="search-form">
+          <div className="app-name">
+            Weather App
+          </div>
+          <div className="city-search-input">
+            <TextField variant="standard" type="text" label="Search for a city" />
+            <Button variant="contained" endIcon={<SearchRounded />} size="large">SUBMIT</Button>
+            <span className="msg" />
+          </div>
+        </form>
+      </section>
+      <section className="cities-section">
+        <ul className="cities-list" />
+      </section>
     </div>
   );
 }
